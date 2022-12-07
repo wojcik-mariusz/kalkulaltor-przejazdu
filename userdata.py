c10f71ce42a0f4a2e383244e927b2622799dfdac
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-07 23:30:03 +0100

    Add get_destination_and_origin func to get coordinates from sys.argv.

--- userdata.py
+++ userdata.py
@@ -8,7 +8,18 @@ from sys import argv
 
 def get_car_name() -> str:
     if len(argv) > 1:
-        car = str(argv[1]).lower()
+        car = str(argv[1]).lower().strip()
+        print(f"Car name: {car}")
         return car
     else:
         return "Default"
+
+
+def get_destination_and_origin() -> dict:
+    coordinates = {}
+    if len(argv) > 3:
+        coordinates['origin_adres'] = argv[2]
+        coordinates['destination_adres'] = argv[3]
+
+    else:
+        raise ValueError("Missed origin or destination coordinates")
