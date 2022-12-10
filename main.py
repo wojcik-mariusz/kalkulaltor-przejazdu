b70771e0e3ad0e8daf32bbc55d058ed8f3235755
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-10 18:15:48 +0100

    Add function to run app

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import NoReturn
+
+from google_maps_api import serialize_data_from_gmaps_api
+from userdata import get_destination_and_origin, get_car_name
+
+
+def app() -> NoReturn:
+    payload = get_destination_and_origin()
+    car = get_car_name()
+    data = serialize_data_from_gmaps_api(
+        destination_address=payload.get("destination_address"),
+        origin_address=payload.get("origin_address"),
+    )
+
+    print(
+        f"""
+    Statistics:
+    Car: {car.title()}
+    Start: {data.get('origin_addresses')}
+    End: {data.get('destination_addresses')}
+    Duration: {data.get('duration')}
+    Length: {data.get('distance_in_m')/1000} km.
+    """
+    )
