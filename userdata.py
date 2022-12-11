6ef2e21d89f8e3e2e885aec0831c278626d6b949
Mariusz W <wojcikk.mariusz@gmail.com>
2022-12-11 23:12:45 +0100

    Clean code using black lib. Also added return to if statement in get_destination_and_origin function.

--- userdata.py
+++ userdata.py
@@ -1,7 +1,7 @@
-#TODO env
-#TODO coordinates
-#TODO validator
-#TODO case when user not specify car name
+# TODO env
+# TODO coordinates
+# TODO validator
+# TODO case when user not specify car name
 
 from sys import argv
 
@@ -9,7 +9,6 @@ from sys import argv
 def get_car_name() -> str:
     if len(argv) > 1:
         car = str(argv[1]).lower().strip()
-        print(f"Car name: {car}")
         return car
     else:
         return "Default"
@@ -18,8 +17,9 @@ def get_car_name() -> str:
 def get_destination_and_origin() -> dict:
     coordinates = {}
     if len(argv) > 3:
-        coordinates['origin_adres'] = argv[2]
-        coordinates['destination_adres'] = argv[3]
+        coordinates["origin_adres"] = argv[2]
+        coordinates["destination_adres"] = argv[3]
+        return coordinates
 
     else:
         raise ValueError("Missed origin or destination coordinates")
