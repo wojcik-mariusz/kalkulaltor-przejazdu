09d5200fa554ae460f569c9805cc8fdcb4d217a4
MFireIt <wojcikk.mariusz@gmail.com>
2022-12-13 21:14:31 +0100

    Add AddressValidationGMaps, not works, raise "missing parameter address in request"

--- google_maps_api.py
+++ google_maps_api.py
@@ -1,6 +1,8 @@
+#TODO AddressValidationGMaps.validate_address
+
 from os import environ
 
-from requests import get
+from requests import get, post
 from dotenv import load_dotenv
 
 
@@ -38,3 +40,51 @@ def serialize_data_from_gmaps_api(
         "distance_in_m": distance_in_m,
         "duration": duration,
     }
+
+
+class AddressValidationGMaps:
+
+    @staticmethod
+    # def validate_address(origin_address, destination_address):
+    def validate_address():
+        url = "https://addressvalidation.googleapis.com/v1:validateAddress"
+        load_dotenv()
+        api_key = environ.get('ADDRESS_VALID_API_KEY')
+        payload = {
+            'address': {
+                'revision': 0,
+                'regionCode': 'PL',
+                'locality': 'Mountain View',
+                'addressLines': ['1600 Amphitheatre Pkwy']
+                # 'addressLines': [origin_address, destination_address]
+            },
+            'previousResponseId':'',
+            'enableUspsCass': True,
+        }
+
+        example_payload = {
+          "address": {
+            "regionCode": "US",
+            "locality": "Mountain View",
+            "administrativeArea": "CA",
+            "postalCode": "94043",
+            "addressLines": ["1600 Amphitheatre Pkwy"],
+          },
+          "enableUspsCass": False
+        }
+
+        parameters = {
+            'key': api_key,
+
+        }
+        response = post(url, json=example_payload, data=parameters)
+        print(response)
+        if response.ok:
+            data = response.json()
+            print(data)
+        else:
+            print(response.json())
+
+
+AddressValidationGMaps.validate_address()
+
